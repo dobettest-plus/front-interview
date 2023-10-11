f77299f89f120172f4a470fe534048e75a32e8d8
liugongming <gongming.liu@bizops.com.cn>
2023-10-11 10:29:03 +0800

    add answer

--- js/convert.js
+++ js/convert.js
@@ -1,51 +1,45 @@
 // 原始 list 如下
-let list =[
-    {id:1,name:'部门A',parentId:0},
-    {id:2,name:'部门B',parentId:0},
-    {id:3,name:'部门C',parentId:1},
-    {id:4,name:'部门D',parentId:1},
-    {id:5,name:'部门E',parentId:2},
-    {id:6,name:'部门F',parentId:3},
-    {id:7,name:'部门G',parentId:2},
-    {id:8,name:'部门H',parentId:4}
+let list = [
+  { id: 1, name: "部门A", parentId: 0 },
+  { id: 2, name: "部门B", parentId: 0 },
+  { id: 3, name: "部门C", parentId: 1 },
+  { id: 4, name: "部门D", parentId: 1 },
+  { id: 5, name: "部门E", parentId: 2 },
+  { id: 6, name: "部门F", parentId: 3 },
+  { id: 7, name: "部门G", parentId: 2 },
+  { id: 8, name: "部门H", parentId: 4 },
 ];
-const result = convert(list)
 
-// 转换后的结果如下
-let results = [
-    {
-      id: 1,
-      name: '部门A',
-      parentId: 0,
-      children: [
-        {
-          id: 3,
-          name: '部门C',
-          parentId: 1,
-          children: [
-            {
-              id: 6,
-              name: '部门F',
-              parentId: 3
-            }, {
-              id: 16,
-              name: '部门L',
-              parentId: 3
-            }
-          ]
-        },
-        {
-          id: 4,
-          name: '部门D',
-          parentId: 1,
-          children: [
-            {
-              id: 8,
-              name: '部门H',
-              parentId: 4
-            }
-          ]
-        }
-      ]
-    }
-];
+// 转换后的结果data.json所示
+
+async function convert(list) {
+  const dfs = (item) => {
+    const children = list.filter((ele) => ele.parentId === item.id);
+    return { ...item, children: children.map((child) => dfs(child)) };
+  };
+  return dfs({ id: 0 });
+}
+convert(list).then((result) => {
+  const fs = require("fs");
+  // fs.writeFileSync("./data.json", JSON.stringify(result));
+});
+async function async1() {
+  console.log("async1 start");//2
+  await async2();
+  console.log("async1 end");//4
+}
+async function async2() {
+  console.log("async2");//3
+}
+console.log("script start");//1
+setTimeout(function () {
+  console.log("setTimeout");//8
+}, 0);
+async1();
+new Promise(function (resolve) {
+  console.log("promise1");//5
+  resolve();
+}).then(function () {
+  console.log("promise2");//7
+});
+console.log("script end");//6
